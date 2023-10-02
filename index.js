399c20738e4bc661404dfb641b0fcdd617a16820
Abhishek5874 <91618311+Abhishek5874@users.noreply.github.com>
2023-10-03 00:50:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// Function to validate email
+function ValidateEmail(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return inputText.match(mailformat) !== null;
+}
+
+document.getElementById('accommodationForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevent default form submission
+
+    const emailInput = document.getElementsByName('email')[0].value;
+    if (!ValidateEmail(emailInput)) {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
+
+    const formData = new FormData(this);
+
+    fetch('/accommodation_inquiry', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log('Response:', data);
+        alert("Form submitted successfully!"); // Alert the user about successful submission
+        // You can redirect to another page or do further handling if needed
+    })
+    .catch(error => console.error('Error:', error));
+});
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'c_strap_db'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.stack);
+        throw err;
+    }
+    console.log('Connected to MySQL database as id', connection.threadId);
+});
+
+app.get('/', (req, res) => {
+    res.send('Root endpoint reached successfully.');
+});
+
+app.get('/test', (req, res) => {
+    res.send('Test endpoint reached successfully.');
+});
+
+app.post('/accommodation_inquiry', (req, res) => {
+    const formData = req.body;
+    console.log('Received form data:', formData);  // Add this line to log the form data
+
+    const { name, email, institution } = formData;
+
+    const query = 'INSERT INTO accommodation_inquiries (name, email, institution) VALUES (?, ?, ?)';
+    connection.query(query, [name, email, institution], (err, result) => {
+        if (err) {
+            console.error('Error inserting data into database:', err.stack);
+            res.status(500).send('Error inserting data into the database');
+            return;
+        }
+        res.send('Accommodation inquiry data successfully submitted to the database!');
+    });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
